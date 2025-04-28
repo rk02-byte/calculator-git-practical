b60513158b9abc9b368a578642ca8483014dae88
rk02-byte <riya.kedia21@st.niituniversity.in>
2025-04-28 17:02:41 +0530

    Add skeleton code for calculator

---
+++ main.py
@@ -0,0 +1,26 @@
+# main.py
+
+def main():
+    print("Simple Calculator")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    choice = int(input("Enter your choice: "))
+    
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    
+    if choice == 1:
+        pass  # To be completed
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
+    else:
+        print("Invalid Choice!")
+
+if __name__ == "__main__":
+    main()
